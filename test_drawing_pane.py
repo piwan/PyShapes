6b64a1c9a9948db95df270cded4a1685e72debf8
Paweł Iwan <piwan@akamai.com>
2019-02-14 15:10:55 +0100

    Add circle shape

--- test_drawing_pane.py
+++ test_drawing_pane.py
@@ -1,12 +1,13 @@
 from drawing_pane import ConsoleDrawingPane
-from shapes import Rectangle, Point, Square
+from shapes import Rectangle, Point, Square, Circle
 
 pane = ConsoleDrawingPane()
 pane.shapes = [
     Rectangle(1, 1, 3, 5, 'R'),
     Point(29, 9),
     Point(0, 0, 'G'),
-    Square(10, 0, 10, 'Y')
+    Square(10, 0, 10, 'Y'),
+    Circle(20, 10, 5)
 ]
 
 print("Before changes:")
@@ -15,5 +16,6 @@ pane.draw()
 pane.shapes[0].move(1, 10)
 pane.shapes[1].move(1, 1)
 pane.shapes[3].is_filled = True
+pane.shapes[4].color = 'R'
 print("After changes:")
 pane.draw()
