a2db87a8d0361a261ad35d91f2061d944e36eca8
Paweł Iwan <piwan@akamai.com>
2019-02-14 14:14:53 +0100

    Add test case to draw Square in ConsoleDrawingPane

--- test_drawing_pane.py
+++ test_drawing_pane.py
@@ -1,10 +1,11 @@
 from drawing_pane import ConsoleDrawingPane
-from shapes import Rectangle, Point
+from shapes import Rectangle, Point, Square
 
 pane = ConsoleDrawingPane()
 pane.shapes = [
     Rectangle(1, 1, 3, 5),
     Point(29, 9),
-    Point(0, 0)
+    Point(0, 0),
+    Square(10, 0, 10)
 ]
 pane.draw()
